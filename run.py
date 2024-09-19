9f68fb77da5fcf40cf8d81731875cfb1e00e2be5
Guwalgiya <hguan7@gatech.edu>
2024-09-19 16:00:38 -0400

    update metadata

--- run.py
+++ run.py
@@ -4,9 +4,5 @@ if __name__ == '__main__':
 
     pipeline.run_pipeline(
         s_input_csv_path  = 'optimization_take_home.csv',
-        s_output_csv_path = 'output.csv',
-        s_video_html_path = 'output_vedio.html',
-
-        ### this can be (1) naive, (2) basic, and (3) final
-        s_solving_method  = 'basic'
+        s_output_csv_path = 'output.csv'
     )
